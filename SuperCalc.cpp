3f4ec680b696eea9265f76bdb77212449e9ee347
Sushruth Pujar <135992844+OnePlateDosa@users.noreply.github.com>
2023-06-11 20:12:03 +0200

    1.5 SuperCalc.cpp
    A previous commit was made, but when invalid characters were added, the code would not run as expected, so the old 1.3 version has been brought back.

--- SuperCalc.cpp
+++ SuperCalc.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <Windows.h>
-// Don't use namespace std, you will get a bunch of namespace conflictions.
-// Just type std:: before everything in namespace std.
-// using namespace std;
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
 
 int main()
 {
 	int digCount;
 
-	std::cout << "How many digits will you be entering? (2 or 3): ";
-	std::cin >> digCount;
+	cout << "How many digits will you be entering? (2 or 3): ";
+	cin >> digCount;
 
 	if (digCount == 2) {
 
@@ -17,36 +18,36 @@ int main()
 		char sign;
 		double dig2;
 
-		std::cout << "What is your fist digit?: " << std::endl;
-		std::cin >> dig1;
-		std::cout << "What is your operator (+, -, *, /): " << std::endl;
-		std::cin >> sign;
-		std::cout << "What is your second digit?: " << std::endl;
-		std::cin >> dig2;
+		cout << endl << "What is your fist digit?: ";
+		cin >> dig1;
+		cout << "What is your operator (+, -, *, /): ";
+		cin >> sign;
+		cout << "What is your second digit?: ";
+		cin >> dig2;
 
 		double result;
-        // Use a switch statment instead of if else, it looks more clean.
-        switch(sign)
-        {
-            case '+':
-                result = dig1 + dig2;
-                break;
-            case '-':
-                result = dig1 - dig2;
-                break;
-            case '*':
-                result = dig1 * dig2;
-                break;
-            case '/':
-                result = dig1 / dig2;
-                break;
-            default:
-                std::cout << "Error, wrong input." << std::endl;
-                break;
-        }
-        std::cout << "The Answer is: " << result << std::endl;
+
+		if (sign == '+') {
+			result = dig1 + dig2;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '-') {
+			result = dig1 - dig2;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '*') {
+			result = dig1 * dig2;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '/') {
+			result = dig1 / dig2;
+			cout << endl << "The answer is: " << result;
+		}
+		else {
+			cout << endl << endl << "Error, its not my fault. You did something wrong";
+		}
 	}
-	else {
+	else if (digCount == 3) {
 
 		double dig1;
 		char sign;
@@ -54,98 +55,103 @@ int main()
 		char sign2;
 		double dig3;
 
-		std::cout << "What is your fist digit?: " << std::endl;
-		std::cin >> dig1;
-		std::cout << "What is your operator (+, -, *, /): " << std::endl;
-		std::cin >> sign;
-		std::cout << "What is your second digit?: " << std::endl;
-		std::cin >> dig2;
-		std::cout << "What is your second operator (+, -, *, /): " << std::endl;
-		std::cin >> sign2;
-		std::cout << "What is your third digit?: " << std::endl;
-		std::cin >> dig3;
+		cout << endl << "What is your fist digit?: ";
+		cin >> dig1;
+		cout << "What is your operator (+, -, *, /): ";
+		cin >> sign;
+		cout << "What is your second digit?: ";
+		cin >> dig2;
+		cout << "What is your second operator (+, -, *, /): ";
+		cin >> sign2;
+		cout << "What is your third digit?: ";
+		cin >> dig3;
 
 		double result;
 
-		switch (sign) {
-			case '+':
-				switch (sign2) {
-					case '+':
-						result = dig1 + dig2 + dig3;
-						break;
-					case '-':
-						result = dig1 + dig2 - dig3;
-						break;
-					case '*':
-						result = dig1 + dig2 * dig3;
-						break;
-					case '/':
-						result = dig1 + dig2 / dig3;
-						break;
-					default:
-                        std::cout << "Error, wrong input" << std::endl;
-                        break;
-				}
-				break;
-			case '-':
-				switch (sign2) {
-					case '+':
-						result = dig1 - dig2 + dig3;
-						break;
-					case '-':
-						result = dig1 - dig2 - dig3;
-						break;
-					case '*':
-						result = dig1 - dig2 * dig3;
-						break;
-					case '/':
-						result = dig1 - dig2 / dig3;
-						break;
-					default:
-                        std::cout << "Error, wrong input" << std::endl;
-                        break;
-				}
-				break;
-			case '*':
-				switch (sign2) {
-					case '+':
-						result = dig1 * dig2 + dig3;
-						break;
-					case '-':
-						result = dig1 * dig2 - dig3;
-						break;
-					case '*':
-						result = dig1 * dig2 * dig3;
-						break;
-					case '/':
-						result = dig1 * dig2 / dig3;
-						break;
-					default:
-                        std::cout << "Error, wrong input" << std::endl;
-                        break;
-				}
-				break;
-			case '/':
-				switch (sign2) {
-					case '+':
-						result = dig1 / dig2 + dig3;
-						break;
-					case '-':
-						result = dig1 / dig2 - dig3;
-						break;
-					case '*':
-						result = dig1 / dig2 * dig3;
-						break;
-					case '/':
-						result = dig1 / dig2 / dig3;
-						break;
-					default:
-                        std::cout << "Error, wrong input" << std::endl;
-						break;
-				}
+		//plus first
+
+		if (sign == '+' && sign2 == '+') {
+			result = dig1 + dig2 + dig3;
+			cout << endl << "The answer is: " << result;
 		}
-               std::cout << "The answer is: " << result << std::endl;
+		else if (sign == '+' && sign2 == '-') {
+			result = dig1 + dig2 - dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '+' && sign2 == '*') {
+			result = dig1 + dig2 * dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '+' && sign2 == '/') {
+			result = dig1 + dig2 / dig3;
+			cout << endl << "The answer is: " << result;
+		}
+
+		//minus first
+
+		else if (sign == '-' && sign2 == '+') {
+			result = dig1 - dig2 + dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '-' && sign2 == '-') {
+			result = dig1 - dig2 - dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '-' && sign2 == '*') {
+			result = dig1 - dig2 * dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '-' && sign2 == '/') {
+			result = dig1 - dig2 / dig3;
+			cout << endl << "The answer is: " << result;
+		}
+
+		//mult first
+
+		else if (sign == '*' && sign2 == '+') {
+			result = dig1 * dig2 + dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '*' && sign2 == '-') {
+			result = dig1 * dig2 - dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '*' && sign2 == '*') {
+			result = dig1 * dig2 * dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '*' && sign2 == '/') {
+			result = dig1 * dig2 / dig3;
+			cout << endl << "The answer is: " << result;
+		}
+
+		//div first
+
+		else if (sign == '/' && sign2 == '+') {
+			result = dig1 / dig2 + dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '/' && sign2 == '-') {
+			result = dig1 / dig2 - dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '/' && sign2 == '*') {
+			result = dig1 / dig2 * dig3;
+			cout << endl << "The answer is: " << result;
+		}
+		else if (sign == '/' && sign2 == '/') {
+			result = dig1 / dig2 / dig3;
+			cout << endl << "The answer is: " << result;
+		}
+
+		else {
+			cout << endl << endl << "Error, its not my fault. You did something wrong";
+		}
 	}
-    Sleep(7000);
-    return 0;
+
+	else {
+		cout << endl << "I refuse. I don't want to do it.";
+	}
+
+	Sleep(7000);
 }
